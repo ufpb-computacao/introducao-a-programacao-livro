a2adfb7b0b1fe45ce848fa951959dcdfa243da66
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-05-24 19:40:36 -0300

    Exemplos de registro

---
+++ livro/capitulos/code/registros/reg_cliente.c
@@ -0,0 +1,8 @@
+struct Cliente {
+  long codigo;
+  char  nome[256];
+  int   cpf[11];
+};
+
+
+
---
+++ livro/capitulos/code/registros/reg_conta.c
@@ -0,0 +1,6 @@
+struct Conta {
+  long numero_da_conta;
+  long codigo_do_cliente;
+  double saldo;
+};
+
