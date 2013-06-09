1c2448f66603b2cbe7d79b7c7663e61a8e954f06
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-06-09 18:09:31 -0300

    imc

--- livro/capitulos/code/registros/imc_calculo.c
+++ livro/capitulos/code/registros/imc_calculo.c
@@ -5,7 +5,7 @@ struct Pessoa {
 	char  sexo; // 'm': masculino, 'f': femino
 	float peso;
 	float altura;
-	long  cpf;
+	unsigned long long  cpf;
 };
 
 #define QUANTIDADE_DE_PESSOAS 3
@@ -16,13 +16,22 @@ int main(){
 	printf("Campos: nome, altura, peso, cpf, sexo\n");
 	for(int i=0; (i < QUANTIDADE_DE_PESSOAS); i++){
 		printf("\nInforme os dados da pessoa(%i): ",i+1);
-		scanf("%s %f %f %l %1c",pessoas[i].nome, &pessoas[i].altura,
+		scanf("%s %f %f %Lu %c",pessoas[i].nome, &pessoas[i].altura,
 			&pessoas[i].peso, &pessoas[i].cpf, &pessoas[i].sexo);
 	}
 
-	printf("\nMatricula\tNome\tMedia\n");
+	printf("\nInforme o CPF da pessoa: ");
+	unsigned long long cpf_localizador;
+	scanf("%Lu",&cpf_localizador);
+
+	printf("\nSexo\tNome\tIMC");
 	for(int i=0; (i < QUANTIDADE_DE_PESSOAS); i++){
-		printf("%s %s\n",pessoas[i].nome,pessoas[i].sexo);
+		if (cpf_localizador == pessoas[i].cpf){
+			float imc = pessoas[i].peso / (pessoas[i].altura * 
+					pessoas[i].altura);
+			printf("\n%c\t%s\t%1.2f\n",pessoas[i].sexo, pessoas[i].nome, imc);
+			break;
+		}
 	}
 
 	getchar();
