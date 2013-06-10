8db047ab894f64263e30c05a9959b0cf320e6be5
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-06-09 22:32:48 -0300

    produtos

---
+++ livro/capitulos/code/registros/supermercado.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+struct Produto {
+  long  codigo;
+  char  nome[100];
+  float preco;
+};
+
+#define QUANTIDADE_DE_PRODUTOS 5
+
+int main(){
+	struct Produto produtos[QUANTIDADE_DE_PRODUTOS];
+
+	printf("Campos: codigo-do-produto nome preco, y\n");
+	for(int i=0; (i < QUANTIDADE_DE_PRODUTOS); i++){
+		printf("\nInforme os dados do produto(%i): ",i+1);
+		scanf("%ld %s %f",&produtos[i].codigo,produtos[i].nome,
+			&produtos[i].preco);
+	}
+
+	for(int i=0; (i < QUANTIDADE_DE_PRODUTOS); i++){
+		printf("\n%ld\t%s R$ %1.2f", produtos[i].codigo,
+			produtos[i].nome,produtos[i].preco);
+	}
+
+	long codigo_digitado;
+	printf("\nInforme o codigo do produto: ");
+	scanf("%ld", &codigo_digitado);
+
+	for(int i=1; (i < QUANTIDADE_DE_PRODUTOS); i++){
+		if (produtos[i].codigo == codigo_digitado) {
+			printf("\nPreço do produto: R$ %1.2f\n", produtos[i].preco);
+		}
+	}
+
+	getchar();
+	return 0;
+}
