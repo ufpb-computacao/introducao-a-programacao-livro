b53752c3591f8a20c073b70130ee4f40655cabf3
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-06-12 10:43:52 -0300

    registro

--- livro/capitulos/code/registros/reg_produto_exemplo.c
+++ livro/capitulos/code/registros/reg_produto_exemplo.c
@@ -9,7 +9,7 @@ struct Produto {
 int main(){
 	struct Produto p;
 
-	scanf("%ld %s %f", &p.codigo, p.nome, &p.preco);
+	scanf("%ld %s %f", &p.codigo, p.nome, &p.preco); <1>
 
 	if (p.preco < 4)
 		printf("\nProduto em promocao: %s R$ %1.2f", p.nome, p.preco);
