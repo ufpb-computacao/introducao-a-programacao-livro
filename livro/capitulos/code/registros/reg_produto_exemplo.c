35d4024bd4023d1dfa666c8dc0447bb0150da804
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-06-11 23:17:34 -0300

    registro

--- livro/capitulos/code/registros/reg_produto_exemplo.c
+++ livro/capitulos/code/registros/reg_produto_exemplo.c
@@ -6,12 +6,10 @@ struct Produto {
 	float preco;
 };
 
-
 int main(){
 	struct Produto p;
 
 	scanf("%ld %s %f", &p.codigo, p.nome, &p.preco);
-
 
 	if (p.preco < 4)
 		printf("\nProduto em promocao: %s R$ %1.2f", p.nome, p.preco);
