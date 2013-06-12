a89ee3b86b71f3a245380cb4c57996af7f589d81
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-06-12 17:40:11 -0300

    registros

--- livro/capitulos/code/registros/reg_atribuicao.c
+++ livro/capitulos/code/registros/reg_atribuicao.c
@@ -13,8 +13,8 @@ typedef struct {
 } Cliente;
 
 int main() {
-	Aluno a =  {15, "Virgulino da Silva", 9.0f, 10.0f};
-	Cliente c = {"Maria Bonita", 72779162201};
+	Aluno a =  {15, "Virgulino da Silva", 9.0f, 10.0f}; // <1>
+	Cliente c = {"Maria Bonita", 72779162201}; // <2>
 
 	printf("Aluno: %s Mat.: %d Nota1: %1.2f Nota2: %1.2f\n", 
 		a.nome, a.matricula, a.nota1, a.nota2);
