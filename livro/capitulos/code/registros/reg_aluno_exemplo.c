b91f06b5acefca13cd931a85e7207d80e5cf0ffa
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-06-12 10:40:10 -0300

    registro

--- livro/capitulos/code/registros/reg_aluno_exemplo.c
+++ livro/capitulos/code/registros/reg_aluno_exemplo.c
@@ -15,7 +15,7 @@ int main(){
 	aluno.nota1 = 8.0; //<1>
 	aluno.nota2 = 9.0; //<1>
 
-	printf("\n%d %s %1.2f %1.2f", aluno.matricula, aluno.nome,
+	printf("\n%d %s %1.2f %1.2f", aluno.matricula, aluno.nome, // <3>
 		aluno.nota1, aluno.nota2);
 
 
