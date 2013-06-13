608eb6c7164fb786e4876ac8ed403f9c619bee62
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@lavid.ufpb.br>
2013-06-13 10:31:48 -0300

    registro-conversa

--- livro/capitulos/code/registros/reg_ponto_exemplo.c
+++ livro/capitulos/code/registros/reg_ponto_exemplo.c
@@ -11,9 +11,9 @@ typedef struct{
 int main(){
 	Ponto pontos[QUANTIDADE_DE_PONTOS]; // <2>
 
-	pontos[0].x = -4;  pontos[0].y = 7;// <3>
-	pontos[1].x = 2; pontos[1].y = -9; // <3>
-	pontos[2].x = 5; pontos[2].y = 3;  // <3>
+	pontos[0].x = -4; pontos[0].y =  7;// <3>
+	pontos[1].x =  2; pontos[1].y = -9;// <3>
+	pontos[2].x =  5; pontos[2].y =  3;// <3>
 
 	for (int i = 0; i < QUANTIDADE_DE_PONTOS ; i++){
 		if(pontos[i].y > 0)
