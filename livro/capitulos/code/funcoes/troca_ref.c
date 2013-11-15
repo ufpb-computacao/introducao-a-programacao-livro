8f42057e618a7a583bb75d7fd7f3744d285a3496
Andrei Formiga <archimedes_siracusa@hotmail.com>
2013-11-15 04:19:51 -0300

    Passagem por valor, exemplo por referencia

--- livro/capitulos/code/funcoes/troca_ref.c
+++ livro/capitulos/code/funcoes/troca_ref.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void troca_variaveis(int a, int b) {
-	int temp = a;
-	a = b;
-	b = temp;
+void troca_variaveis(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 
-	printf("Dentro de troca_variaveis: a = %d, b = %d\n", a, b);
+	printf("Dentro de troca_variaveis: a = %d, b = %d\n", *a, *b);
 }
 
 int main() {
@@ -13,6 +13,6 @@ int main() {
 	int y = 7;
 
 	printf("Antes da troca: x = %d, y = %d\n", x, y);
-	troca_variaveis(x, y);
+	troca_variaveis(&x, &y);
 	printf("Depois da troca: x = %d, y = %d\n", x, y);
 }
