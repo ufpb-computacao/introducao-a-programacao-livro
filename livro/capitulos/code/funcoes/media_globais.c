a88e091608c80b03c326020a41e4facf3cd9e8af
Andrei Formiga <archimedes_siracusa@hotmail.com>
2013-11-14 23:27:20 -0300

    Exemplos de variaveis locais e globais

--- livro/capitulos/code/funcoes/media_globais.c
+++ livro/capitulos/code/funcoes/media_globais.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
+float nota1, nota2, nota3;   // <1>
+
 float calc_media() {
-	return (nota1 + nota2 + nota3) / 3.0;  // <1>
+	return (nota1 + nota2 + nota3) / 3.0;  // <2>
 }
 
 int main() {
-	float nota1, nota2, nota3;
-
 	printf("Entre as três notas: ");
-	scanf("%f %f %f", &nota1, &nota2, &nota3);
+	scanf("%f %f %f", &nota1, &nota2, &nota3);  // <3> 
 
 	printf("Media: %f\n", calc_media());
 
--- livro/capitulos/code/funcoes/media_param2.c
+++ livro/capitulos/code/funcoes/media_param2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-float calc_media() {
-	return (nota1 + nota2 + nota3) / 3.0;  // <1>
+float calc_media(float n1, float n2, float n3) {
+	return (n1 + n2 + n3) / 3.0;
 }
 
 int main() {
@@ -10,7 +10,7 @@ int main() {
 	printf("Entre as três notas: ");
 	scanf("%f %f %f", &nota1, &nota2, &nota3);
 
-	printf("Media: %f\n", calc_media());
+	printf("Media: %f\n", calc_media(nota1, nota2, nota3));
 
 	return 0;
 }
